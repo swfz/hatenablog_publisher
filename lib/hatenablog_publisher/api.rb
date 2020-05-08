c0a8a0304734cfa1c009fb8d17a6a2074616c812
swfz <sawafuji.09@gmail.com>
2020-05-09 03:13:16 +0900

    fix: rubocop

--- lib/hatenablog_publisher/api.rb
+++ lib/hatenablog_publisher/api.rb
@@ -3,32 +3,32 @@ require 'json'
 
 module HatenablogPublisher
   class Api
-     attr_reader :client, :header
+    attr_reader :client, :header
 
     def initialize(site)
-      unless (ENV['HATENABLOG_CONSUMER_KEY'] && ENV['HATENABLOG_CONSUMER_SECRET'] )
+      if !ENV['HATENABLOG_CONSUMER_KEY'] || !ENV['HATENABLOG_CONSUMER_SECRET']
         raise 'Not Found environment [HATENABLOG_CONSUMER_KEY|HATENABLOG_CONSUMER_SECRET]'
       end
-      unless (ENV['HATENABLOG_ACCESS_TOKEN'] && ENV['HATENABLOG_ACCESS_TOKEN_SECRET'] )
+      if !ENV['HATENABLOG_ACCESS_TOKEN'] || !ENV['HATENABLOG_ACCESS_TOKEN_SECRET']
         raise 'Not Found environment [HATENABLOG_ACCESS_TOKEN|HATENABLOG_ACCESS_TOKEN_SECRET]'
       end
 
       @header = {
-          'Accept' => 'application/xml',
-          'Content-Type' => 'application/xml'
+        'Accept' => 'application/xml',
+        'Content-Type' => 'application/xml'
       }
 
       consumer = OAuth::Consumer.new(
-          ENV['HATENABLOG_CONSUMER_KEY'],
-          ENV['HATENABLOG_CONSUMER_SECRET'],
-          site: site,
-          timeout: 300
+        ENV['HATENABLOG_CONSUMER_KEY'],
+        ENV['HATENABLOG_CONSUMER_SECRET'],
+        site: site,
+        timeout: 300
       )
 
       @client = OAuth::AccessToken.new(
-          consumer,
-          ENV['HATENABLOG_ACCESS_TOKEN'],
-          ENV['HATENABLOG_ACCESS_TOKEN_SECRET']
+        consumer,
+        ENV['HATENABLOG_ACCESS_TOKEN'],
+        ENV['HATENABLOG_ACCESS_TOKEN_SECRET']
       )
     end
 
