5fb4131d76c9c9d232954091c550b63e1f1749be
swfz <sawafuji.09@gmail.com>
2020-05-09 01:56:15 +0900

    feat: ciのベース

--- spec/hatenablog_publisher_spec.rb
+++ spec/hatenablog_publisher_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe HatenablogPublisher do
   it "has a version number" do
     expect(HatenablogPublisher::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
