e68bf4b316041642f0ea0f7a158beb48243e08ab
swfz <sawafuji.09@gmail.com>
2020-05-09 18:23:46 +0900

    feat: requestの前後でログを取るような関数を用意

--- lib/hatenablog_publisher/request_logger.rb
+++ lib/hatenablog_publisher/request_logger.rb
@@ -1,12 +1,21 @@
 require 'pry'
 module HatenablogPublisher
   module RequestLogger
-    def log(identifier, target)
+
+    def with_logging_request(identifier, request_body, &block)
+      log("#{identifier}-request", request_body)
+      res = yield
+      log("#{identifier}-response", res.body)
+
+      res
+    end
+
+    def log(identifier, text)
       Dir.mkdir('tmp') unless Dir.exist?('tmp')
 
       classname = self.class.to_s.demodulize.downcase
       filepath = "tmp/hatenablog_publisher-#{classname}-#{identifier}.xml"
-      File.write(filepath, target)
+      File.write(filepath, text)
       p "[Info] #{classname}: #{identifier}. output log -> #{filepath}."
     end
   end
