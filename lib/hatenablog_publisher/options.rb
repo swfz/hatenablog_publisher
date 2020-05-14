a186a5d16be8858cc27044369109d2dfa2484890
swfz <sawafuji.09@gmail.com>
2020-05-14 12:22:03 +0900

    fix: add error pattern. when config key is nil

--- lib/hatenablog_publisher/options.rb
+++ lib/hatenablog_publisher/options.rb
@@ -17,7 +17,8 @@ module HatenablogPublisher
 
     def valid_or_raise
       config_keys = to_h.keys
-      unless (lacking_keys = REQUIRE_KEYS.reject { |key| config_keys.include?(key) }).empty?
+      key_is_set = ->(key) { config_keys.include?(key) && !to_h[key].nil? }
+      unless (lacking_keys = REQUIRE_KEYS.reject { |key| key_is_set.call(key) }).empty?
         raise "Following keys are not setup. #{lacking_keys.map(&:to_s)}"
       end
 
