979619ec01eece2e2fa6a56109a5c7d1322df54d
swfz <sawafuji.09@gmail.com>
2020-05-23 01:40:07 +0900

    feat: 投稿時刻のコントロール

--- lib/hatenablog_publisher/context.rb
+++ lib/hatenablog_publisher/context.rb
@@ -1,10 +1,11 @@
 require 'yaml'
+require 'time'
 require 'front_matter_parser'
 require 'active_support/core_ext/hash'
 
 module HatenablogPublisher
   class Context
-    attr_reader :title, :categories, :text, :hatena
+    attr_reader :title, :categories, :updated, :text, :hatena
 
     def initialize(io)
       @io = io
@@ -40,19 +41,36 @@ module HatenablogPublisher
     def add_entry_context(entry)
       @hatena ||= {}
       @hatena[:id] = entry['id'].first.split('-').last
+      @updated = time_to_s(entry['updated'].first)
+    end
+
+    def time_to_s(str)
+      Time.parse(str).strftime('%Y-%m-%dT%H:%M:%S')
+    end
+
+    def metadata
+      {
+        title: @title,
+        category: @categories,
+        updated: @updated,
+        hatena: @hatena
+      }
     end
 
     private
 
     def write_context
-      @io.write(title: @title, category: @categories, hatena: @hatena, text: @text)
+      @io.write(metadata, @text)
     end
 
     def read_context
       data, text = @io.read
 
+      updated = data[:updated].to_s.empty? ? '' : time_to_s(data[:updated])
+
       @text = text
       @categories = data[:category]
+      @updated = updated
       @title = data[:title]
       @hatena = data[:hatena] || {}
     end
