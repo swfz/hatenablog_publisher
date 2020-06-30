7f8db85c89820d78df6a8e9ab36cff72a676f014
swfz <sawafuji.09@gmail.com>
2020-07-01 06:56:04 +0900

    fix: coverage出すために読み込み順変更

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,17 @@
 require "bundler/setup"
-require "hatenablog_publisher"
 
 require 'simplecov'
-SimpleCov.start
+SimpleCov.command_name 'Rspec'
+SimpleCov.start do
+  track_files "lib/**/*.rb"
+  load_profile "test_frameworks"
+  enable_coverage :branch
+end
 
 require 'codecov'
 SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
+require "hatenablog_publisher"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
